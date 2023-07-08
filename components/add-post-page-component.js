e743a2ab2354663cc502bef6691157a4385e64c0
Sofia Iakovleva <sofyakovleva95@gmail.com>
2023-07-08 13:09:49 +0300

    Добавлена верстка страницы добавления комментариев

--- components/add-post-page-component.js
+++ components/add-post-page-component.js
@@ -1,15 +1,29 @@
+import { renderHeaderComponent } from "./header-component.js";
+
 export function renderAddPostPageComponent({ appEl, onAddPostClick }) {
   const render = () => {
     // TODO: Реализовать страницу добавления поста
     const appHtml = `
     <div class="page-container">
-      <div class="header-container"></div>
-      Cтраница добавления поста
+    <div class="header-container"></div>
+     <div class="form">
+      <h3 class="form-title">
+       Добавить пост
+        </h3>
+      <div class="form-inputs">
+          <div class="upload-image-container"></div>
+          <p>Опишите фотографию:</p>
+          <textarea class="input textarea" rows="4"></textarea>
+          
       <button class="button" id="add-button">Добавить</button>
     </div>
   `;
 
     appEl.innerHTML = appHtml;
+
+    renderHeaderComponent({
+      element: document.querySelector(".header-container"),
+    });
 
     document.getElementById("add-button").addEventListener("click", () => {
       onAddPostClick({
