fe347ac642a525ebe2bd924471e7f111b0d8b3b5
Sofia Iakovleva <sofyakovleva95@gmail.com>
2023-07-08 19:37:56 +0300

    Реализована функция добавления постов

--- components/add-post-page-component.js
+++ components/add-post-page-component.js
@@ -1,15 +1,17 @@
 import { renderHeaderComponent } from "./header-component.js";
+import { renderUploadImageComponent } from "./upload-image-component.js";
 
 export function renderAddPostPageComponent({ appEl, onAddPostClick }) {
+
+  let imageUrl = "";
+
   const render = () => {
     // TODO: Реализовать страницу добавления поста
     const appHtml = `
     <div class="page-container">
     <div class="header-container"></div>
      <div class="form">
-      <h3 class="form-title">
-       Добавить пост
-        </h3>
+      <h3 class="form-title">Добавить пост</h3>
       <div class="form-inputs">
           <div class="upload-image-container"></div>
           <p>Опишите фотографию:</p>
@@ -25,10 +27,31 @@ export function renderAddPostPageComponent({ appEl, onAddPostClick }) {
       element: document.querySelector(".header-container"),
     });
 
+    renderUploadImageComponent({
+      element: appEl.querySelector(".upload-image-container"),
+      onImageUrlChange(newImageUrl) {
+        imageUrl = newImageUrl;
+      },
+    });
+
+    const textInput = appEl.querySelector(".textarea");
+
     document.getElementById("add-button").addEventListener("click", () => {
+      if (!imageUrl) {
+        alert("Выберите фото");
+        return;
+      }
+      if (!textInput.value) {
+        alert("Не заполнено описание фото")
+        return;
+      }
       onAddPostClick({
-        description: "Описание картинки",
-        imageUrl: "https://image.png",
+        description: textInput.value
+          .replaceAll("&", "&amp;")
+          .replaceAll("<", "&lt;")
+          .replaceAll(">", "&gt;")
+          .replaceAll('"', "&quot;"),
+        imageUrl,
       });
     });
   };
